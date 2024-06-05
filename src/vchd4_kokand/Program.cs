bcdec3ba485cb199f7a2e8b6e192e900dbe7ed12
Farkhad-K <farhodbek.kamolov2303@gmail.com>
2024-06-05 12:38:14 +0500

    Hamma componentlarniham implement qilmadim qolganlarini o'ziz qilib olarsiz

--- src/vchd4_kokand/Program.cs
+++ src/vchd4_kokand/Program.cs
@@ -4,8 +4,5 @@ using vchd4_kokand;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
